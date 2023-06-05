615d88ae139094a7ff8b6e83953397061e0bc4ea
Ahmed Ali <61229648+itxahmedali@users.noreply.github.com>
2023-06-05 06:30:06 -0700

    working on captain sign up

--- src/screens/captain/DriverLogin.js
+++ src/screens/captain/DriverLogin.js
@@ -1,16 +1,183 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { useAppContext } from '../../context/AppContext';
-
+import React, {useEffect, useState, useContext} from 'react';
+import {
+  View,
+  StyleSheet,
+  Animated,
+  Text,
+} from 'react-native';
+import {
+  gray,
+  primaryHeadingColor,
+  purple,
+  white,
+  KumbhSansExtraBold,
+  screenWidth,
+  black,
+  emailRegex,
+} from '../../constants/Index';
+import {moderateScale} from 'react-native-size-matters';
+import {useNavigation} from '@react-navigation/native';
+import {AppContext} from '../../context/AppContext';
+import {
+  Heading,
+  Button,
+  Input,
+  TopLeftCircleProp,
+} from '../../components/Index';
 const CapatainLogin = () => {
-  const { state,setState } = useAppContext();
-
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const GirlAnimation = new Animated.Value(screenWidth + 250);
+  const navigation = useNavigation();
+  const {setToken, setUser} = useContext(AppContext);
+  const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true); 
+  useEffect(() => {
+    startAnimations();
+  }, []);
+  useEffect(() => {
+    if (email === '' || password === '' || !emailRegex.test(email)) {
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
+    }, [email, password]);
+  const login = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setToken(true);
+      setUser({email})
+      setLoading(false);
+    }, 1500);
+  };
+  const startAnimations = () => {
+    Animated.timing(GirlAnimation, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
   return (
-    <View>
-      <Text>CapatainLogin</Text>
-      <Button onPress={() => setState(state == "Ahmed" ? "Ali" :"Ahmed")} title="Context Check State" />
+    <View style={styles.container}>
+      <TopLeftCircleProp />
+      <View style={styles.headingBox}>
+        <Heading
+          style={null}
+          text="Sign in to continue"
+          fontSize={moderateScale(40)}
+          fontFamily={KumbhSansExtraBold}
+          color={primaryHeadingColor}
+          textAlign="left"
+        />
+        <View style={styles.InputBox}>
+          <Input
+            placeholderTextColor={black}
+            style={{marginBottom: 16}}
+            placeholder="Email"
+            value={email}
+            setValue={setEmail}
+            type="text"
+          />
+          <Input
+            placeholderTextColor={black}
+            style={{marginBottom: 16}}
+            placeholder="Password"
+            value={password}
+            setValue={setPassword}
+            type="password"
+          />
+        </View>
+        <Button
+          style={null}
+          fontSize={moderateScale(12)}
+          backgroundColor={null}
+          color={purple}
+          text="Forgot Password?"
+          padding={moderateScale(0)}
+          textAlign="right"
+          borderRadius={moderateScale(0)}
+          width="100%"
+          onPress={() => navigation.navigate('ForgotPassword')}
+        />
+        <View style={styles.signInButtonContainer}>
+          <Button
+            disabled={disabled}
+            loading={loading}
+            style={null}
+            fontSize={moderateScale(14)}
+            backgroundColor={purple}
+            color={white}
+            text="Sign In"
+            padding={moderateScale(10)}
+            textAlign="center"
+            borderRadius={moderateScale(100)}
+            width="50%"
+            onPress={() => login()}
+          />
+        </View>
+        <View style={styles.dontHaveBox}>
+          <Text style={styles.dontHaveBoxText}>Don't have an account?</Text>
+          <Button
+            style={null}
+            fontSize={moderateScale(12)}
+            backgroundColor={null}
+            color={purple}
+            text="Sign Up Now!"
+            padding={moderateScale(0)}
+            textAlign="center"
+            borderRadius={moderateScale(0)}
+            width="30%"
+            onPress={() => navigation.navigate('CapatainSignup')}
+          />
+        </View>
+      </View>
+      <Animated.Image
+        style={[styles.GirlProp, {transform: [{translateX: GirlAnimation}]}]}
+        resizeMode="contain"
+        source={require('../../../assets/Images/captainLoginProp.png')}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+  },
+  headingBox: {
+    width: moderateScale(screenWidth - 100),
+  },
+  InputBox: {
+    marginTop: moderateScale(20),
+  },
+  signInButtonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: moderateScale(15),
+  },
+  dontHaveBox: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    marginVertical: moderateScale(15),
+  },
+  dontHaveBoxText: {
+    color: gray,
+  },
+  GirlProp: {
+    position: 'absolute',
+    bottom: moderateScale(10),
+    right: moderateScale(100),
+    width: moderateScale(200),
+    resizeMode: 'contain',
+    height: moderateScale(180),
+    zIndex:-1
+  }
+});
 
 export default CapatainLogin;
--- src/screens/captain/DriverSignUp.js
+++ src/screens/captain/DriverSignUp.js
@@ -1,16 +1,311 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { AppContext, useAppContext } from '../../context/AppContext';
-
+import React, {useContext, useEffect, useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Animated,
+  KeyboardAvoidingView,
+  Text,
+  ScrollView,
+} from 'react-native';
+import {
+  gray,
+  primaryHeadingColor,
+  purple,
+  white,
+  KumbhSansExtraBold,
+  screenWidth,
+  black,
+} from '../../constants/Index';
+import {moderateScale} from 'react-native-size-matters';
+import {useNavigation} from '@react-navigation/native';
+import {AppContext} from '../../context/AppContext';
+import {
+  Heading,
+  Button,
+  Input,
+  TopLeftCircleProp,
+  BottomCircleProp,
+} from '../../components/Index';
 const CapatainSignUp = () => {
-  const { state,setState } = useAppContext(AppContext);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contact, setContact] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [carMaker, setCarMaker] = useState('');
+  const [model, setModel] = useState('');
+  const [carYear, setCarYear] = useState('');
+  const [carColor, setCarColor] = useState('');
+  const [service, setService] = useState('');
+  const [carCapacity, setCarCapacity] = useState('');
+  const CarAnimation = new Animated.Value(-screenWidth + 250);
+  const navigation = useNavigation();
+  const {setToken, setUser} = useContext(AppContext);
+  useEffect(() => {
+    startAnimations();
+  }, []);
 
+  const startAnimations = () => {
+    Animated.sequence([
+      Animated.timing(CarAnimation, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }),
+      Animated.spring(CarAnimation, {
+        toValue: 1,
+        friction: 4,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
+  const signUp = () => {
+    setUser({firstName, lastName, contact, password, email});
+    setToken(true);
+  };
   return (
-    <View>
-      <Text>CapatainSignUp</Text>
-      <Button onPress={() => setState(state == "Ahmed" ? "Ali" :"Ahmed")} title="Context Check State" />
-    </View>
+      <ScrollView contentContainerStyle={styles.container}>
+      <TopLeftCircleProp />
+        <View style={styles.headingBox}>
+          <Heading
+            style={null}
+            text="Fill this form to
+            complete sign up!"
+            fontSize={moderateScale(20)}
+            fontFamily={KumbhSansExtraBold}
+            color={primaryHeadingColor}
+            textAlign="left"
+          />
+          <View style={styles.InputBox}>
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="First Name"
+                value={firstName}
+                setValue={setFirstName}
+                type="text"
+              />
+            </View>
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="Last Name"
+                value={lastName}
+                setValue={setLastName}
+                type="text"
+              />
+            </View>
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="Email"
+                value={email}
+                setValue={setEmail}
+                type="text"
+              />
+            </View>
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="Contact"
+                value={contact}
+                setValue={setContact}
+                type="text"
+              />
+            </View>
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="City"
+                value={city}
+                setValue={setCity}
+                type="text"
+              />
+            </View>
+
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="State"
+                value={state}
+                setValue={setState}
+                type="text"
+              />
+            </View>
+
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="Car Maker"
+                value={carMaker}
+                setValue={setCarMaker}
+                type="text"
+              />
+            </View>
+
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="Model"
+                value={model}
+                setValue={setModel}
+                type="text"
+              />
+            </View>
+
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="Car Year"
+                value={carYear}
+                setValue={setCarYear}
+                type="text"
+              />
+            </View>
+
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="Car Color"
+                value={carColor}
+                setValue={setCarColor}
+                type="text"
+              />
+            </View>
+
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="Service"
+                value={service}
+                setValue={setService}
+                type="text"
+              />
+            </View>
+
+            <View style={styles.input}>
+              <Input
+                placeholderTextColor={black}
+                style={[styles.input, {marginBottom: 16}]}
+                placeholder="Car Capacity"
+                value={carCapacity}
+                setValue={setCarCapacity}
+                type="text"
+              />
+            </View>
+          </View>
+          <View style={styles.signInButtonContainer}>
+            <Button
+              style={null}
+              fontSize={moderateScale(14)}
+              backgroundColor={purple}
+              color={white}
+              text="Register Now"
+              padding={moderateScale(10)}
+              textAlign="center"
+              borderRadius={moderateScale(100)}
+              width="50%"
+              onPress={() => signUp()}
+            />
+          </View>
+          <View style={styles.dontHaveBox}>
+            <Text style={styles.dontHaveBoxText}>Already have an account?</Text>
+            <Button
+              style={null}
+              fontSize={moderateScale(12)}
+              backgroundColor={null}
+              color={purple}
+              text="Sign In!"
+              padding={moderateScale(0)}
+              textAlign="center"
+              borderRadius={moderateScale(0)}
+              width="30%"
+              onPress={() => navigation.navigate('Login')}
+            />
+          </View>
+        </View>
+      <Animated.Image
+        style={[
+          styles.CarProp,
+          {
+            transform: [
+              {
+                translateY: CarAnimation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [-screenWidth + 250, 0],
+                }),
+              },
+              {
+                scale: CarAnimation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 1],
+                }),
+              },
+            ],
+          },
+        ]}
+        resizeMode="contain"
+        source={require('../../../assets/Images/captainSignUpProp.png')}
+      />
+      <BottomCircleProp />
+      </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    position: 'relative',
+    padding: moderateScale(30),
+  },
+  headingBox: {
+    width: moderateScale(screenWidth - 100),
+  },
+  InputBox: {
+    marginTop: moderateScale(20),
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  input: {
+    width: moderateScale(screenWidth / 2 - 60),
+    marginVertical: moderateScale(10),
+  },
+  signInButtonContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: moderateScale(15),
+  },
+  dontHaveBox: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    marginVertical: moderateScale(15),
+  },
+  dontHaveBoxText: {
+    color: gray,
+  },
+  CarProp: {
+    width: moderateScale(200),
+    resizeMode: 'contain',
+    height: moderateScale(180),
+  },
+});
 
 export default CapatainSignUp;
