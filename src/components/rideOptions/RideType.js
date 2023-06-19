eb85fd15699ed1d1d430ff8e50bde5de57f42baf
Ahmed Ali <61229648+itxahmedali@users.noreply.github.com>
2023-06-19 06:19:36 -0700

    promo input issue resolve

--- src/components/rideOptions/RideType.js
+++ src/components/rideOptions/RideType.js
@@ -1,23 +1,42 @@
-import React, {useContext} from 'react';
-import {
-  View,
-  KeyboardAvoidingView,
-  StyleSheet,
-  FlatList,
-  Platform,
-} from 'react-native';
-import {AppContext} from '../../context/AppContext';
-import {CarDetail, Heading, Promo, RideDetail} from '../../components/Index';
-import {KumbhSansBold, rides, screenWidth, white} from '../../constants/Index';
-import {moderateScale} from 'react-native-size-matters';
+import React, { useContext, useState, useEffect, useRef } from 'react';
+import { View, KeyboardAvoidingView, StyleSheet, FlatList, Platform, Keyboard, Animated } from 'react-native';
+import { AppContext } from '../../context/AppContext';
+import { CarDetail, Heading, Promo, RideDetail } from '../../components/Index';
+import { KumbhSansBold, rides, screenWidth, white } from '../../constants/Index';
+import { moderateScale } from 'react-native-size-matters';
 
 const RideType = () => {
-  const {rideDetails} = useContext(AppContext);
+  const { rideDetails } = useContext(AppContext);
+  const [isKeyboardActive, setKeyboardActive] = useState(false);
+  const carListOpacity = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+      setKeyboardActive(true);
+      animateCarList(false);
+    });
+
+    const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => {
+      setKeyboardActive(false);
+      animateCarList(true);
+    });
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+
+  const animateCarList = (isVisible) => {
+    Animated.timing(carListOpacity, {
+      toValue: isVisible ? 1 : 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      enabled
-      style={styles.container}>
+    <View style={styles.container}>
       <View style={styles.headingContainer}>
         <Heading
           text="Select Your Type"
@@ -31,17 +50,19 @@ const RideType = () => {
       <View style={styles.rideDetailContainer}>
         <RideDetail rideDetails={rideDetails} />
       </View>
-      <View style={styles.carListContainer}>
-        <FlatList
-          data={rides}
-          keyExtractor={item => item.id}
-          renderItem={({item, index}) => (
-            <CarDetail key={index} car={item} index={index} select={true} />
-          )}
-        />
-      </View>
+      {!isKeyboardActive && (
+        <Animated.View style={[styles.carListContainer, { opacity: carListOpacity }]}>
+          <FlatList
+            data={rides}
+            keyExtractor={item => item.id}
+            renderItem={({ item, index }) => (
+              <CarDetail key={index} car={item} index={index} select={true} />
+            )}
+          />
+        </Animated.View>
+      )}
       <Promo />
-    </KeyboardAvoidingView>
+    </View>
   );
 };
 
@@ -50,7 +71,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingBottom: moderateScale(30, 0.1),
-    position: 'relative',
   },
   headingContainer: {
     width: '100%',
